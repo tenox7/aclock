80de7874e3538f2ee8ff7662ac0df18a6ed93cfb
Giovanni Nunes <giovanni.nunes@gmail.com>
2018-02-21 08:13:27 -0300

    small fixes

--- sources/aclock-msxdos/aclock.c
+++ sources/aclock-msxdos/aclock.c
@@ -1,23 +1,24 @@
 /*
  * aclock - ascii clock for MSX, MSX2, MSX2+ & MSX turbo R machines
  *
- * Copyright (c) 2002 Antoni Sawicki <tenox@tenox.tc>
- * Version 1.8 (z80p3cpm); Dublin, June 2002
+ * Copyright (c) 2002-2018 by Antoni Sawicki <tenox@tenox.tc>
+ * Copyright (c) 2018 by Giovanni Nunes <giovanni.nunes@gmail.com>
  *
- * Compilation:
+ * Version 1.8 (sdcc-z80-msxdos); Campinas, February 2018
+ *
+ * To compile:
  * sdcc -I../msx -mz80 --no-std-crt0 --data-loc 0 --code-loc 0x107
  *      ../msx/crt0msx_msxdos.rel ../msx/putchar.rel ../msx/getchar.rel
  *      ../msx/dos.rel ../msx/conio.rel get_time.rel aclock.c
  *
- * To SDCC backend for MSXDOS go to:
- *    http://msx.atlantes.org/index_en.html#sdccmsxdos
+ * Download the SDCC backend for MSXDOS in:
+ * http://msx.atlantes.org/index_en.html#sdccmsxdos
  */
 
 #include <stdio.h>
-#include <float.h>
 
-#define  LINLEN	       (* (char *) 0xf3b0)
-#define  CRTCNT        (* (char *) 0xf3b1)
+#define  LINLEN	(* (char *) 0xf3b0)
+#define  CRTCNT (* (char *) 0xf3b1)
 
 static char * get_time(char *c);
 
@@ -47,7 +48,7 @@ void main(void){
 
 	char digital[15];
 
-	static char dummy[]="HMSC";	// this is a very ugly dummy!
+	static char dummy[]="HMSC";	// this is very ugly!
 	char *time;
 
 	float ratio;
@@ -65,7 +66,7 @@ void main(void){
 		-0.866027, -0.809019, -0.743147, -0.669133, -0.587788,
 		-0.500003, -0.406740, -0.309021, -0.207915, -0.104532 };
 
-	// ...and cossine.
+	// ...and cosine.
 	float cos_t[] = { -1.000000, -0.994522, -0.978148, -0.951058,
 		-0.913547, -0.866028, -0.809020, -0.743148, -0.669134,
 		-0.587789, -0.500004, -0.406741, -0.309022, -0.207917,
@@ -78,16 +79,16 @@ void main(void){
 		-0.499997, -0.587783, -0.669128, -0.743143, -0.809015,
 		-0.866024, -0.913544, -0.951055, -0.978147, -0.994521 };
 
-	int max_x = LINLEN;		// total of columns in text mode
-	int max_y = CRTCNT;		// total of lines in text mode
+	int max_x = LINLEN;		// number of columns in current text mode
+	int max_y = CRTCNT;		// number of lines in current text mode
 
 	int cen_x,cen_y,hand,i,j,smax,x,y;
 
 	if ( max_x > 64 )		// MSX2/2+/turbo R supports 80 columns
 		ratio = 2.0;		// text mode.
 	else {
-		if ( max_x > 48 )	// MSX2+ or turbo R machines can operate
-			ratio = 1.5;	// in KANJI mode.
+		if ( max_x > 48 )	// MSX2+/turbo R can operates in KANJI
+			ratio = 1.5;	// mode and using 64 columns text mode
 		else
 			ratio = 1.0;	// for 40/32 columns text mode.
 	}
@@ -114,7 +115,8 @@ void main(void){
 
 			draw_text(cen_x-5, max_y/4, ".:ACLOCK:.");
 
-			sprintf(digital,"[%02d:%02d:%02d]",time[0],time[1],time[2]);
+			sprintf(digital,"[%02d:%02d:%02d]",
+					time[0],time[1],time[2]);
 
 			draw_text(cen_x-5, 4*max_y/5, digital);
 
@@ -129,9 +131,12 @@ void main(void){
 			else
 				time[0]=5*time[0];
 
-			draw_hand(2*hand/3,'h',cen_x,cen_y,ratio,sin_t[time[0]],cos_t[time[0]]);
-			draw_hand(hand-2,'m',cen_x,cen_y,ratio,sin_t[time[1]],cos_t[time[1]]);
-			draw_hand(hand-1,'.',cen_x,cen_y,ratio,sin_t[time[2]],cos_t[time[2]]);
+			draw_hand(2*hand/3,'h',cen_x,cen_y,ratio,
+					sin_t[time[0]],cos_t[time[0]]);
+			draw_hand(hand-2,'m',cen_x,cen_y,ratio,
+					sin_t[time[1]],cos_t[time[1]]);
+			draw_hand(hand-1,'.',cen_x,cen_y,ratio,
+					sin_t[time[2]],cos_t[time[2]]);
 
 		}
 		j = time[2];
