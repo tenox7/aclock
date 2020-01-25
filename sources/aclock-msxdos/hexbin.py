e6f6db6e5ff494b582527d0f7370fa74da93a6b1
Giovanni Nunes <giovanni.nunes@gmail.com>
2020-01-25 12:52:05 -0300

    add Python 3 support and improve 'hexbin' tool.

--- sources/aclock-msxdos/hexbin.py
+++ sources/aclock-msxdos/hexbin.py
@@ -1,14 +1,24 @@
-#!/usr/bin/env python2
-import bincopy
-import sys
+#!/usr/bin/env python3
+from __future__ import print_function
+from sys import argv
 
-def main(argv):
-    inputfile = argv[0]
-    f = bincopy.File()
-    with open(inputfile, "r") as fin:
-        f.add_ihex(fin)
-    print f.as_binary()
+import bincopy
+
+def main():
+    """
+    Main function
+    """
+    try:
+        inputfile = argv[1]
+        outputfile = argv[2]
+
+    except IndexError:
+        print("Use {} <inputfile> <outputfile".format(argv[0]))
+
+    ihx_file = bincopy.BinFile(inputfile)
+
+    open(outputfile, "wb").write(ihx_file.as_binary())
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
-
+    main()
